1febcb2e4ae78780a6b6576c05b5d20fd3b40afe
kixelated <kixelated@gmail.com>
2023-05-24 17:20:44 -0700

    Serve the certificate fingerprint over HTTP (#20)
    It's a bloated solution, but it's required for local development until
    Chrome supports self-signed WebTransport CAs.

--- README.md
+++ README.md
@@ -6,41 +6,40 @@ See the [Warp draft](https://datatracker.ietf.org/doc/draft-lcurley-warp/).
 This repository is a Rust server that supports both contribution (ingest) and distribution (playback).
 It requires a client, such as [moq-js](https://github.com/kixelated/moq-js).
 
-## Requirements
+## Setup
 
--   **rust**: duh
--   **ffmpeg**: (optional) used to generate fragmented media
--   **go**: (optional) used to generate self-signed certificates
--   **openssl**: (options) ...also used to generate self-signed certificates
-
-## Media
+### Media
 
 This demo simulates a live stream by reading a file from disk and sleeping based on media timestamps. Obviously you should hook this up to a real live stream to do anything useful.
 
-Download your favorite media file and convert it to fragmented MP4, by default `media/fragmented.mp4`:
+Download your favorite media file and convert it to fragmented MP4.
+This requires [ffmpeg](https://ffmpeg.org/)
 
 ```
 wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4 -O media/source.mp4
 ./media/generate
 ```
 
-## Certificates
+### Certificates
 
 Unfortunately, QUIC mandates TLS and makes local development difficult.
 If you have a valid certificate you can use it instead of self-signing.
 
-Otherwise, we use [mkcert](https://github.com/FiloSottile/mkcert) to install a self-signed CA:
+Use [mkcert](https://github.com/FiloSottile/mkcert) to generate a self-signed certificate.
+Unfortunately, this currently requires Go in order to [fork](https://github.com/FiloSottile/mkcert/pull/513) the tool.
 
 ```
 ./cert/generate
 ```
 
-With no arguments, the server will generate self-signed cert using this root CA.
-This certificate is only valid for _2 weeks_ due to how WebTransport performs certificate fingerprinting.
+Unfortunately, WebTransport in Chrome currently (May 2023) doesn't verify certificates using the root CA.
+The workaround is to use the `serverFingerprints` options, which requires the certificate MUST be only valid for at most **14 days**.
+This is also why we're using a fork of mkcert, because it generates certificates valid for years by default.
+This limitation will be removed once Chrome uses the system CA for WebTransport.
 
-## Server
+## Usage
 
-`The Warp server supports WebTransport, pushing media over streams once a connection has been established.
+Run the server:
 
 ```
 cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
-use warp::{session, transport};
+use std::io::BufReader;
+use std::net::SocketAddr;
+use std::{fs::File, sync::Arc};
+
+use moq::{session, transport};
 
 use clap::Parser;
+use ring::digest::{digest, SHA256};
+use warp::Filter;
 
 /// Search for a pattern in a file and display the lines that contain it.
-#[derive(Parser)]
+#[derive(Parser, Clone)]
 struct Cli {
 	/// Listen on this address
 	#[arg(short, long, default_value = "[::]:4443")]
 	addr: String,
 
 	/// Use the certificate file at this path
-	#[arg(short, long, default_value = "../cert/localhost.crt")]
+	#[arg(short, long, default_value = "cert/localhost.crt")]
 	cert: String,
 
 	/// Use the private key at this path
-	#[arg(short, long, default_value = "../cert/localhost.key")]
+	#[arg(short, long, default_value = "cert/localhost.key")]
 	key: String,
 
 	/// Use the media file at this path
-	#[arg(short, long, default_value = "../media/fragmented.mp4")]
+	#[arg(short, long, default_value = "media/fragmented.mp4")]
 	media: String,
 }
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
 	env_logger::init();
 
-	let args = Cli::parse();
+	let moq_args = Cli::parse();
+	let http_args = moq_args.clone();
 
+	// TODO return result instead of panicing
+	tokio::task::spawn(async move { run_transport(moq_args).unwrap() });
+
+	run_http(http_args).await
+}
+
+// Run the WebTransport server using quiche.
+fn run_transport(args: Cli) -> anyhow::Result<()> {
 	let server_config = transport::Config {
 		addr: args.addr,
 		cert: args.cert,
@@ -35,4 +51,40 @@ fn main() -> anyhow::Result<()> {
 
 	let mut server = transport::Server::<session::Session>::new(server_config).unwrap();
 	server.run()
+}
+
+// Run a HTTP server using Warp
+// TODO remove this when Chrome adds support for self-signed certificates using WebTransport
+async fn run_http(args: Cli) -> anyhow::Result<()> {
+	let addr: SocketAddr = args.addr.parse()?;
+
+	// Read the PEM certificate file
+	let crt = File::open(&args.cert)?;
+	let mut crt = BufReader::new(crt);
+
+	// Parse the DER certificate
+	let certs = rustls_pemfile::certs(&mut crt)?;
+	let cert = certs.first().expect("no certificate found");
+
+	// Compute the SHA-256 digest
+	let fingerprint = digest(&SHA256, cert.as_ref());
+	let fingerprint = hex::encode(fingerprint.as_ref());
+	let fingerprint = Arc::new(fingerprint);
+
+	let cors = warp::cors().allow_any_origin();
+
+	// What an annoyingly complicated way to serve a static String
+	// I spent a long time trying to find the exact way of cloning and dereferencing the Arc.
+	let routes = warp::path!("fingerprint")
+		.map(move || (*(fingerprint.clone())).clone())
+		.with(cors);
+
+	warp::serve(routes)
+		.tls()
+		.cert_path(args.cert)
+		.key_path(args.key)
+		.run(addr)
+		.await;
+
+	Ok(())
 }
