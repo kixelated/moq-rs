f447def9a2b3262aa2f33acbff662b24fcce6249
Luke Curley <kixelated@gmail.com>
2023-07-18 17:15:47 -0700

    temp

--- moq-transport-quinn/src/object.rs
+++ moq-transport-quinn/src/object.rs
@@ -1,9 +1,8 @@
 use anyhow::Context;
-use bytes::BytesMut;
+use bytes::{BytesMut};
 use moq_transport::{Decode, Encode, Object};
 use tokio::task::JoinSet;
 
-use super::{RecvStream, SendStream};
 use webtransport_quinn::Session;
 
 pub struct SendObjects {
@@ -83,3 +82,57 @@ impl RecvObjects {
 		Ok((header, stream))
 	}
 }
+
+pub type SendStream = webtransport_quinn::SendStream;
+pub type RecvStream = webtransport_quinn::RecvStream;
+
+/*
+// Unfortunately we need a wrapper for reading, because the moq-transport::Coding API means we read too much of the stream.
+pub struct RecvStream {
+	stream: webtransport_quinn::RecvStream,
+	buffer: Bytes,
+}
+
+impl RecvStream {
+	pub fn new(stream: webtransport_quinn::RecvStream, buffer: Bytes) -> Self {
+		Self { stream, buffer }
+	}
+
+	pub async fn read(&mut self, dst: &mut [u8]) -> Result<Option<usize>, webtransport_quinn::ReadError> {
+		if !self.buffer.is_empty() {
+			let size = std::cmp::min(dst.len(), self.buffer.len());
+			self.buffer.copy_to_slice(&mut dst[..size]);
+			Ok(Some(size))
+		} else {
+			self.stream.read(dst).await
+		}
+	}
+
+	pub async fn read_exact(&mut self, mut buf: &mut [u8]) -> Result<(), webtransport_quinn::ReadExactError> {
+		if self.buffer.is_empty() {
+			let size = std::cmp::min(buf.len(), self.buffer.len());
+			self.buffer.copy_to_slice(&mut buf[..size]);
+			buf = &mut buf[size..];
+		}
+
+		self.stream.read_exact(&mut buf).await
+	}
+
+	pub async fn read_chunk(
+		&mut self,
+		max_length: usize,
+		ordered: bool,
+	) -> Result<Option<quinn::Chunk>, webtransport_quinn::ReadError> {
+		if self.buffer.is_empty() {
+		}
+	}
+
+	pub async fn read_chunks(&mut self, bufs: &mut [Bytes]) -> Result<Option<usize>, webtransport_quinn::ReadError> {}
+
+	pub async fn read_to_end(&mut self, size_limit: usize) -> Result<Vec<u8>, webtransport_quinn::ReadToEndError> {}
+
+	pub fn stop(&mut self, error_code: u32) -> Result<(), webtransport_quinn::UnknownStream> {
+		self.stream.stop(error_code)
+	}
+}
+*/
