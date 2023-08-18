fbd84f7a85d87664f42b585084e75233b65a4fe2
Mike English <mike.english@gmail.com>
2023-08-18 16:35:21 -0400

    Move CLI config to separate file

---
+++ moq-pub/src/cli.rs
@@ -0,0 +1,27 @@
+use clap::{Parser, ValueEnum};
+use std::net;
+
+#[derive(Parser, Clone)]
+#[command(arg_required_else_help(true))]
+pub struct Cli {
+	#[arg(long, hide_short_help = true, default_value = "[::]:0")]
+	pub bind_address: net::SocketAddr,
+
+	#[arg(short, long, default_value = "https://localhost:4443")]
+	pub uri: http::uri::Uri,
+
+	#[arg(short, long, required = true, value_parser=input_parser)]
+	input: InputValues,
+}
+
+fn input_parser(s: &str) -> Result<InputValues, String> {
+	if s == "-" {
+		return Ok(InputValues::Stdin);
+	}
+	Err("The only currently supported input value is: '-' (stdin)".to_string())
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum InputValues {
+	Stdin,
+}
--- moq-pub/src/main.rs
+++ moq-pub/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
-use clap::{CommandFactory, Parser, ValueEnum};
 use std::{net, process::exit, str::FromStr};
+use clap::Parser;
 use tokio::task::JoinSet;
 
 mod session_runner;
@@ -15,33 +15,11 @@ use log_viewer::*;
 mod media;
 use media::*;
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum InputValues {
-	Stdin,
-}
+mod cli;
+use cli::*;
 
 // TODO: clap mangen
 // TODO: clap complete
-
-#[derive(Parser, Clone)]
-#[command(arg_required_else_help(true))]
-struct Cli {
-	#[arg(long, hide_short_help = true, default_value = "[::]:0")]
-	bind_address: net::SocketAddr,
-
-	#[arg(short, long, default_value = "https://localhost:4443")]
-	uri: http::uri::Uri,
-
-	#[arg(short, long, required = true, value_parser=input_parser)]
-	input: InputValues,
-}
-
-fn input_parser(s: &str) -> Result<InputValues, String> {
-	if s == "-" {
-		return Ok(InputValues::Stdin);
-	}
-	Err("The only currently supported input value is: '-' (stdin)".to_string())
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
