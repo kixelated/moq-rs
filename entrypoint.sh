049dd07d34fe041ee7262a726b6ea4a8b081aad6
Mike English <mike.english@gmail.com>
2023-09-06 12:46:51 -0400

    Update Dockerfile paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env sh
 
 mkdir cert
+# Nothing to see here...
 echo "$MOQ_CRT" | base64 -d > cert/moq-demo.crt
 echo "$MOQ_KEY" | base64 -d > cert/moq-demo.key
 
-# while true; do
-#     echo "."
-#     sleep 5
-# done
-
-RUST_LOG=info ./moq-demo --cert cert/moq-demo.crt --key cert/moq-demo.key
+RUST_LOG=info ./moq-quinn --cert cert/moq-demo.crt --key cert/moq-demo.key
