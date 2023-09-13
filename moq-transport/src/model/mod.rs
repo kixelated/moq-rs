c643a45c31f1b9934f6c13fed1e4344592c31fa4
Luke Curley <kixelated@gmail.com>
2023-09-12 18:22:32 -0700

    Very minor.

--- moq-transport/src/model/mod.rs
+++ moq-transport/src/model/mod.rs
@@ -1,7 +1,7 @@
 //! Allows a publisher to push updates, automatically caching and fanning it out to any subscribers.
 //!
 //! The naming scheme doesn't match the spec because it's vague and confusing.
-//! The hierarchy is: [broker] -> [broadcast] -> [track] -> [segment] -> [bytes::Bytes]
+//! The hierarchy is: [broker] -> [broadcast] -> [track] -> [segment] -> [Bytes](bytes::Bytes)
 
 pub mod broadcast;
 pub mod broker;
