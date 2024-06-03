fb362bf0e043c8f277bf47eb345b4f53cfb78948
Luke Curley <kixelated@gmail.com>
2024-06-03 06:25:34 -0700

    Revert "filter DNS query results to only include addresses that our quic endpoint can use (#166)"
    This breaks my moq-pub client and the bbb stream in production.
    I think it's using IPv6 to connect to a IPv4 address which is valid.
    
    This reverts commit 1230cd6d6728e0de077bde4ca5e7b21a652a0c63.

--- moq-native/src/quic.rs
+++ moq-native/src/quic.rs
@@ -190,15 +190,12 @@ impl Client {
 		let host = url.host().context("invalid DNS name")?.to_string();
 		let port = url.port().unwrap_or(443);
 
-		let local_addr = self.quic.local_addr()?.ip();
-		let own_protocol_ipv4 = local_addr.to_canonical().is_ipv4();
-
 		// Look up the DNS entry.
 		let addr = tokio::net::lookup_host((host.clone(), port))
 			.await
 			.context("failed DNS lookup")?
-			.find(|s| own_protocol_ipv4 == s.ip().to_canonical().is_ipv4())
-			.context("no DNS entries with matching IP version")?;
+			.next()
+			.context("no DNS entries")?;
 
 		let connection = self.quic.connect_with(config, addr, &host)?.await?;
 
