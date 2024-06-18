def31846a2f809653b29eead650fc5bbe2e9aa7a
Mike English <mike.english@gmail.com>
2024-06-18 14:35:10 -0700

    Add basic parsing for object status
    We don't _do_ anything with this information yet, but now we can at
    least parse it.

--- moq-transport/src/data/object.rs
+++ moq-transport/src/data/object.rs
@@ -1,5 +1,39 @@
 use crate::coding::{Decode, DecodeError, Encode, EncodeError};
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ObjectStatus {
+	Object = 0x0,
+	ObjectDoesNotExist = 0x1,
+	GroupDoesNotExist = 0x2,
+	EndOfGroup = 0x3,
+	EndOfTrack = 0x4,
+}
+
+impl Decode for ObjectStatus {
+	fn decode<B: bytes::Buf>(r: &mut B) -> Result<Self, DecodeError> {
+		match u64::decode(r)? {
+			0x0 => Ok(Self::Object),
+			0x1 => Ok(Self::ObjectDoesNotExist),
+			0x2 => Ok(Self::GroupDoesNotExist),
+			0x3 => Ok(Self::EndOfGroup),
+			0x4 => Ok(Self::EndOfTrack),
+			_ => Err(DecodeError::InvalidObjectStatus),
+		}
+	}
+}
+
+impl Encode for ObjectStatus {
+	fn encode<W: bytes::BufMut>(&self, w: &mut W) -> Result<(), EncodeError> {
+		match self {
+			Self::Object => (0x0_u64).encode(w),
+			Self::ObjectDoesNotExist => (0x1_u64).encode(w),
+			Self::GroupDoesNotExist => (0x2_u64).encode(w),
+			Self::EndOfGroup => (0x3_u64).encode(w),
+			Self::EndOfTrack => (0x4_u64).encode(w),
+		}
+	}
+}
+
 #[derive(Clone, Debug)]
 pub struct ObjectHeader {
 	// The subscribe ID.
@@ -16,6 +50,9 @@ pub struct ObjectHeader {
 
 	// The send order, where **smaller** values are sent first.
 	pub send_order: u64,
+
+	// The object status
+	pub object_status: ObjectStatus,
 }
 
 impl Decode for ObjectHeader {
@@ -26,6 +63,7 @@ impl Decode for ObjectHeader {
 			group_id: u64::decode(r)?,
 			object_id: u64::decode(r)?,
 			send_order: u64::decode(r)?,
+			object_status: ObjectStatus::decode(r)?,
 		})
 	}
 }
@@ -37,6 +75,7 @@ impl Encode for ObjectHeader {
 		self.group_id.encode(w)?;
 		self.object_id.encode(w)?;
 		self.send_order.encode(w)?;
+		self.object_status.encode(w)?;
 
 		Ok(())
 	}
