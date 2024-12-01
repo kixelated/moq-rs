255652d7f26b95d09db6d81559cb09cf4610b1e4
Luke Curley <kixelated@gmail.com>
2024-11-30 20:33:59 -0800

    fmt

--- moq-gst/src/sink/mod.rs
+++ moq-gst/src/sink/mod.rs
@@ -4,15 +4,10 @@ use gst::prelude::*;
 mod imp;
 
 glib::wrapper! {
-    pub struct MoqSink(ObjectSubclass<imp::MoqSink>) @extends gst_base::BaseSink, gst::Element, gst::Object;
+	pub struct MoqSink(ObjectSubclass<imp::MoqSink>) @extends gst_base::BaseSink, gst::Element, gst::Object;
 }
 
 pub fn register(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
-    env_logger::init();
-    gst::Element::register(
-        Some(plugin),
-        "moqsink",
-        gst::Rank::NONE,
-        MoqSink::static_type(),
-    )
+	env_logger::init();
+	gst::Element::register(Some(plugin), "moqsink", gst::Rank::NONE, MoqSink::static_type())
 }
