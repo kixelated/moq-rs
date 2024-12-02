aa89ed22aab81dc25b18985c1b67dc476adf0412
Luke Curley <kixelated@gmail.com>
2024-12-02 14:27:30 -0800

    Fix npm again.

--- moq-web/src/index.ts
+++ moq-web/src/index.ts
@@ -1,6 +1,2 @@
 // Export the library
 export { Watch } from "./watch";
-
-// Export the Web Components
-export { MoqVideoElement } from "./element/video";
-export { MoqKarpElement } from "./element/karp";
