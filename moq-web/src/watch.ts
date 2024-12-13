e0cf6bd30dcff70b7583d60c485eac04a3cf0756
Luke Curley <kixelated@gmail.com>
2024-12-12 22:15:54 -0800

    Delay initializing the worker.

--- moq-web/src/watch.ts
+++ moq-web/src/watch.ts
@@ -1,29 +1,29 @@
 import * as Comlink from "comlink";
-import { init } from "./main";
+import { init } from "./bridge";
 import type * as Api from "./worker";
 
 export class Watch {
-	#inner: Promise<Comlink.Remote<Api.Watch>>;
+	#bridge: Promise<Comlink.Remote<Api.Watch>>;
 
 	constructor(src: string) {
-		this.#inner = init.then((api) => api.watch(src));
+		this.#bridge = init().then((api) => api.watch(src));
 	}
 
 	async pause(value: boolean) {
-		await (await this.#inner).pause(value);
+		await (await this.#bridge).pause(value);
 	}
 
 	async volume(value: number) {
-		await (await this.#inner).volume(value);
+		await (await this.#bridge).volume(value);
 	}
 
 	async render(value: HTMLCanvasElement | OffscreenCanvas) {
 		const canvas = value instanceof HTMLCanvasElement ? value.transferControlToOffscreen() : value;
 
-		await (await this.#inner).render(Comlink.transfer(canvas, [canvas]));
+		await (await this.#bridge).render(Comlink.transfer(canvas, [canvas]));
 	}
 
 	async close() {
-		await (await this.#inner).close();
+		await (await this.#bridge).close();
 	}
 }
