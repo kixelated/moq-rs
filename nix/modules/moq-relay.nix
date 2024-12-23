8d3eb9a69d6a3ce4fe42a65336270ce4f2a3c49a
Rishi Desai <rishi@saronic.com>
2024-12-23 14:41:11 -0600

    feat: support running moq-relay module in dev mode

--- nix/modules/moq-relay.nix
+++ nix/modules/moq-relay.nix
@@ -10,6 +10,13 @@ in
 {
   options.services.moq-relay = {
     enable = lib.mkEnableOption "moq-relay";
+    dev = {
+      enable = lib.mkEnableOption "dev";
+      tls_url = lib.mkOption {
+        type = lib.types.str;
+        description = "URL for the self signed cert";
+      };
+    };
     port = lib.mkOption {
       type = lib.types.port;
       default = 443;
@@ -35,22 +42,6 @@ in
         :::
       '';
     };
-    tls = {
-      keyPath = lib.mkOption {
-        type = lib.types.path;
-        example = "/var/lib/acme/moq-relay.example.org/key.pem";
-        description = ''
-          Path to TLS private key
-        '';
-      };
-      certPath = lib.mkOption {
-        type = lib.types.path;
-        example = "/var/lib/acme/moq-relay.example.org/cert.pem";
-        description = ''
-          Path to TLS certificate
-        '';
-      };
-    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -61,7 +52,9 @@ in
       serviceConfig = {
         User = cfg.user;
         Group = cfg.group;
-        ExecStart = "${pkgs.moq.moq-relay}/bin/moq-relay --bind [::]:${builtins.toString cfg.port} --tls-cert ${cfg.tls.certPath} --tls-key ${cfg.tls.keyPath}";
+        ExecStart = builtins.concatStringsSep " " ([
+          "${pkgs.moq.moq-relay}/bin/moq-relay --bind [::]:${builtins.toString cfg.port}"
+        ] ++ lib.optionals cfg.dev.enable [ "--dev --tls-self-sign ${cfg.dev.tls_url} --cluster-node ${cfg.dev.tls_url} --tls-disable-verify" ]);
         Restart = "on-failure";
         RestartSec = "1";
 
