9be4964397d8eee4d5369c6b4c4558f0398a9262
Luke Curley <kixelated@gmail.com>
2025-02-12 11:58:34 -0800

    Prioritize send streams.

--- moq-transfork/src/session/publisher.rs
+++ moq-transfork/src/session/publisher.rs
@@ -3,9 +3,7 @@ use std::collections::{hash_map, HashMap};
 use futures::{stream::FuturesUnordered, StreamExt};
 
 use crate::{
-	message,
-	model::{GroupConsumer, Track, TrackConsumer},
-	Announced, AnnouncedConsumer, AnnouncedProducer, Error, Path, RouterConsumer,
+	message, model::{GroupConsumer, Track, TrackConsumer}, Announced, AnnouncedConsumer, AnnouncedProducer, Error, GroupOrder, Path, RouterConsumer,
 };
 
 use moq_async::{spawn, FuturesExt, Lock, OrClose};
@@ -151,9 +149,10 @@ impl Publisher {
 				Some(group) = track.next_group().transpose() => {
 					let mut group = group?;
 					let session = self.session.clone();
+					let priority = Self::stream_priority(track.priority, track.order, group.sequence);
 
 					tasks.push(async move {
-						let res = Self::serve_group(session, subscribe.id, &mut group).await;
+						let res = Self::serve_group(session, subscribe.id, priority, &mut group).await;
 						(group, res)
 					});
 				},
@@ -190,13 +189,17 @@ impl Publisher {
 		Ok(())
 	}
 
-	#[tracing::instrument("group", skip_all, fields(?subscribe, sequence = group.sequence))]
+	#[tracing::instrument("group", skip_all, fields(?subscribe, ?priority, sequence = group.sequence))]
 	pub async fn serve_group(
 		mut session: web_transport::Session,
 		subscribe: u64,
+		priority: i32,
 		group: &mut GroupConsumer,
 	) -> Result<(), Error> {
+		// TODO open streams in priority order to help with MAX_STREAMS flow control issues.
 		let mut stream = Writer::open(&mut session, message::DataType::Group).await?;
+		stream.set_priority(priority);
+
 		tracing::trace!("serving");
 
 		Self::serve_group_inner(subscribe, group, &mut stream)
@@ -299,5 +302,46 @@ impl Publisher {
 			Some(router) => router.subscribe(track).await,
 			None => Err(Error::NotFound),
 		}
+	}
+
+	// Quinn takes a i32 priority.
+	// We do our best to distill 70 bits of information into 32 bits, but overflows will happen.
+	// Specifically, group sequence 2^24 will overflow and be incorrectly prioritized.
+	// But even with a group per frame, it will take ~6 days to reach that point.
+	// TODO The behavior when two tracks share the same priority is undefined. Should we round-robin?
+	fn stream_priority(track_priority: i8, group_order: GroupOrder, group_sequence: u64) -> i32 {
+		let sequence = (group_sequence as u32) & 0xFFFFFF;
+		(track_priority as i32) << 24 | match group_order {
+			GroupOrder::Asc => sequence as i32,
+			GroupOrder::Desc => (0xFFFFFF - sequence) as i32,
+		}
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn stream_priority() {
+		let assert = |track_priority, group_order, group_sequence, expected| {
+			assert_eq!(Publisher::stream_priority(track_priority, group_order, group_sequence), expected);
+		};
+
+		const U24: i32 = (1 << 24) - 1;
+
+		// NOTE: The lower the value, the higher the priority.
+		assert(-1, GroupOrder::Asc, 0, -U24-1);
+		assert(-1, GroupOrder::Asc, 50, -U24+49);
+		assert(-1, GroupOrder::Desc, 50, -51);
+		assert(-1, GroupOrder::Desc, 0, -1);
+		assert(0, GroupOrder::Asc, 0, 0);
+		assert(0, GroupOrder::Asc, 50, 50);
+		assert(0, GroupOrder::Desc, 50, U24 - 50);
+		assert(0, GroupOrder::Desc, 0, U24);
+		assert(1, GroupOrder::Asc, 0, U24+1);
+		assert(1, GroupOrder::Asc, 50, U24+51);
+		assert(1, GroupOrder::Desc, 50, 2*U24-49);
+		assert(1, GroupOrder::Desc, 0, 2*U24+1);
 	}
 }
